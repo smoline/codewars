0b9346353f9c5900c7c726a8b96fe9b1ef4fa268
Sherilyn Moline <smoline@gmail.com>
2017-04-21 18:28:05 -0400

    Jenny-secret and volume-cuboid

---
+++ ruby/Jenny-secret.rb
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/jennys-secret-message/train/ruby
+
+def greet(name)
+  if name == 'Johnny'
+    return "Hello, my love!"
+  else
+    return "Hello, #{name}!"
+  end
+end
---
+++ ruby/volume-cuboid.rb
@@ -0,0 +1,5 @@
+# https://www.codewars.com/kata/volume-of-a-cuboid/train/ruby
+
+def get_volume_of_cuboid(length, width, height)
+  length * width * height
+end
