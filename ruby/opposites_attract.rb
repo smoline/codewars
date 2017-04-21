e15c6cf646c077c74a6c711404a6ac455d45cd09
Sherilyn Moline <smoline@gmail.com>
2017-04-21 14:13:00 -0400

    opposites_attract.rb

---
+++ ruby/opposites_attract.rb
@@ -0,0 +1,11 @@
+require 'ap'
+
+# Opposites Attract
+def lovefunc( flower1, flower2 )
+  flower1.even? && flower2.odd? || flower1.odd? && flower2.even?
+end
+
+answer = lovefunc(1,4)
+ap answer
+answer2 = lovefunc(2,2)
+ap answer
