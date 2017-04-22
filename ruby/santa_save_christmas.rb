53b38c510004a2bb1f82335766bc386affe56745
Sherilyn Moline <smoline@gmail.com>
2017-04-22 18:51:23 -0400

    santa_save_christmas.rb

---
+++ ruby/santa_save_christmas.rb
@@ -0,0 +1,21 @@
+# https://www.codewars.com/kata/can-santa-save-christmas/train/ruby
+
+require "ap"
+
+# Can Santa save Christmas?
+def determineTime(durations)
+  durations.map do |duration|
+    hours, minutes, seconds = duration.split(":").map { |duration_string| duration_string.to_i }
+    seconds + 60 * minutes + 60 * 60 * hours
+  end.inject(0, "+") <= 86400
+end
+
+durations = ["02:30:02", "05:30:02", "10:08:10", "03:10:10", "02:40:20"]
+can_he = determineTime(durations)
+ap can_he
+# true
+
+durations = ["06:00:00","12:00:00","06:30:00"]
+can_he = determineTime(durations)
+ap can_he
+# false
