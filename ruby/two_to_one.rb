324c5ebccb93b9a8fe2c8ba06185c6ff37cf7512
Sherilyn Moline <smoline@gmail.com>
2017-04-22 18:47:22 -0400

    two_to_one.rb

---
+++ ruby/two_to_one.rb
@@ -0,0 +1,29 @@
+# https://www.codewars.com/kata/two-to-one/train/ruby
+
+require 'ap'
+
+def longest(string1, string2)
+  longest_string = ()
+  array1 = (string1.chars + string2.chars).sort
+  array1.each do |char|
+    if longest_string == nil
+      longest_string = char
+    end
+    unless longest_string.include?(char)
+      longest_string << char
+    end
+  end
+  return longest_string
+end
+
+string1 = "aretheyhere"
+string2 = "yestheyarehere"
+answer = longest(string1, string2)
+ap answer
+# "aehrsty"
+
+string1 = "loopingisfunbutdangerous"
+string2 = "lessdangerousthancoding"
+answer = longest(string1, string2)
+ap answer
+# "abcdefghilnoprstu"
