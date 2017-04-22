bdd5f79c3080f5b796fcd6652cbc140bde1ad6e6
Sherilyn Moline <smoline@gmail.com>
2017-04-22 17:33:15 -0400

    last.rb

---
+++ ruby/last.rb
@@ -0,0 +1,26 @@
+# https://www.codewars.com/kata/last/train/ruby
+
+require 'ap'
+
+def last(*list, x)
+  case list.empty?
+  when true
+    case x
+    when Array, String then x[-1]
+    else x
+    end
+  else x
+  end
+end
+
+answer = last([1,2,3,4,5])
+ap answer
+
+answer = last("abcde")
+ap answer
+
+answer = last(1, "b", 3, "d", 5)
+ap answer
+
+answer = last(5)
+ap answer
