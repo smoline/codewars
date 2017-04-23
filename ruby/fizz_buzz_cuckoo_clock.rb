6d569d9839c984b1d2277c99e73588390cbfacdb
Sherilyn Moline <smoline@gmail.com>
2017-04-22 20:08:31 -0400

    fizz_buzz_cuckoo_clock.rb

---
+++ ruby/fizz_buzz_cuckoo_clock.rb
@@ -0,0 +1,56 @@
+# https://www.codewars.com/kata/fizz-buzz-cuckoo-clock/train/ruby
+
+require 'ap'
+
+def fizz_buzz_cuckoo_clock(time)
+  array = time.split(':')
+  minutes = array[1].to_i
+  hours = array[0].to_i
+  if hours > 12
+    hours -= 12
+  elsif hours == 0
+    hours = 12
+  end
+  if minutes % 3 == 0 && minutes % 5 != 0
+    "Fizz"
+  elsif minutes % 3 != 0 && minutes % 5 == 0
+    "Buzz"
+  elsif minutes % 3 == 0 && minutes % 5 == 0 && minutes != 0 && minutes != 30
+    "Fizz Buzz"
+  elsif minutes == 0
+    new_string = "Cuckoo"
+    while hours - 1 > 0
+      new_string << " Cuckoo"
+      hours -= 1
+    end
+    return new_string
+  elsif minutes == 30
+    "Cuckoo"
+  else
+    "tick"
+  end
+end
+
+answer = fizz_buzz_cuckoo_clock("13:34")
+ap answer
+# "tick"
+
+answer = fizz_buzz_cuckoo_clock("21:00")
+ap answer
+# "Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo"
+
+answer = fizz_buzz_cuckoo_clock("11:15")
+ap answer
+# "Fizz Buzz"
+
+answer = fizz_buzz_cuckoo_clock("14:30")
+ap answer
+# "Cuckoo"
+
+answer = fizz_buzz_cuckoo_clock("08:55")
+ap answer
+# "Buzz"
+
+answer = fizz_buzz_cuckoo_clock("03:03")
+ap answer
+# "Fizz"
