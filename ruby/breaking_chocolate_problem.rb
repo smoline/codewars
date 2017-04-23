017a5e24e8fdddbdb70aada61d3257c1c3e1598c
Sherilyn Moline <smoline@gmail.com>
2017-04-23 14:18:31 -0400

    breaking_chocolate_problem.rb

---
+++ ruby/breaking_chocolate_problem.rb
@@ -0,0 +1,27 @@
+# https://www.codewars.com/kata/breaking-chocolate-problem/train/ruby
+
+require 'ap'
+
+def break_chocolate(n, m)
+  if n > m
+    (m - 1) + (n - 1) * m
+  elsif n < m
+    (n - 1) + (m - 1) * n
+  elsif n + m == 0
+    return 0
+  else
+    (n - 1) + (m - 1) * n
+  end
+end
+
+answer = break_chocolate(5, 5)
+ap answer
+# 24
+
+answer = break_chocolate(7, 4)
+ap answer
+# 27
+
+answer = break_chocolate(10, 1)
+ap answer
+# 9
