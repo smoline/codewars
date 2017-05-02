1e99112d9a07578792fb296f63dd7dd57035714f
Sherilyn Moline <smoline@gmail.com>
2017-05-01 20:12:07 -0400

    even_or_odd.js

---
+++ javascript/even_or_odd.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/even-or-odd/train/javascript
+
+function even_or_odd(number) {
+  return number % 2 ? "Odd" : "Even"
+}
+
+console.log(even_or_odd(2))
+// "Even"
+
+console.log(even_or_odd(7))
+// "Odd"
