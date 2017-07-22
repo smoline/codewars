8fca8bf9922d306294bc2dd5b4cd0b34eceb9a47
Sherilyn Moline <smoline@gmail.com>
2017-07-22 10:57:17 -0400

    Go 8kyu Multiply

---
+++ go/8kyu-multiply.go
@@ -0,0 +1,13 @@
+// http://www.codewars.com/kata/multiply/train/go
+
+package main
+
+import "fmt"
+
+func Multiply(a, b int) int {
+  return a * b
+}
+
+func main() {
+	fmt.Println(Multiply(42, 13))
+}
