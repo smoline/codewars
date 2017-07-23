e7683be7b6437c6cff6ba31c6ab86a039b1cab68
Sherilyn Moline <smoline@gmail.com>
2017-07-23 11:34:16 -0400

    Regex codewars

---
+++ ruby/6kyu_create_phone_number.rb
@@ -0,0 +1,28 @@
+# https://www.codewars.com/kata/create-phone-number/train/ruby
+
+require 'ap'
+
+def createPhoneNumber(numbers)
+  return numbers.join.gsub!(/^(\d{3})(\d{3})(\d{4})$/, '(\1) \2-\3')
+end
+
+answer = createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+ap answer
+# => returns "(123) 456-7890"
+
+answer = createPhoneNumber([7, 8, 3, 5, 6, 1, 2, 4, 6, 2])
+ap answer
+# => returns "(783) 561-2462"
+
+# Another Way
+def createPhoneNumber(numbers)
+  return "(#{numbers[0]}#{numbers[1]}#{numbers[2]}) #{numbers[3]}#{numbers[4]}#{numbers[5]}-#{numbers[6]}#{numbers[7]}#{numbers[8]}#{numbers[9]}"
+end
+
+answer = createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+ap answer
+# => returns "(123) 456-7890"
+
+answer = createPhoneNumber([7, 8, 3, 5, 6, 1, 2, 4, 6, 2])
+ap answer
+# => returns "(783) 561-2462"
---
+++ ruby/6kyu_valid_phone_number.rb
@@ -0,0 +1,58 @@
+# https://www.codewars.com/kata/valid-phone-number/train/ruby
+
+require 'ap'
+
+def validPhoneNumber(phoneNumber)
+  if phoneNumber.match (/[a-z]/)
+    return false
+  elsif phoneNumber.match /\(\d{3}\) \d{3}-\d{4}/
+    return true
+  else
+    return false
+  end
+end
+
+answer = validPhoneNumber("(123) 456-7890")
+ap answer
+# true
+
+answer = validPhoneNumber("(1234) 123-8765")
+ap answer
+# false
+
+answer = validPhoneNumber("(abc) 123-8765")
+ap answer
+# false
+
+answer = validPhoneNumber("(098) 123 8765")
+ap answer
+# false
+
+answer = validPhoneNumber("(098) 123-8765 a")
+ap answer
+# false
+
+# Another Way
+def validPhoneNumber(phoneNumber)
+  !!phoneNumber[/\A\(\d{3}\)\s\d{3}-\d{4}\z/]
+end
+
+answer = validPhoneNumber("(123) 456-7890")
+ap answer
+# true
+
+answer = validPhoneNumber("(1234) 123-8765")
+ap answer
+# false
+
+answer = validPhoneNumber("(abc) 123-8765")
+ap answer
+# false
+
+answer = validPhoneNumber("(098) 123 8765")
+ap answer
+# false
+
+answer = validPhoneNumber("(098) 123-8765 a")
+ap answer
+# false
