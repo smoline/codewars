7e62a437acdecb5aafba5779c3ba5ab41b679cc3
Sherilyn Moline <smoline@gmail.com>
2017-07-24 10:05:06 -0400

    Stop spinning my words

---
+++ ruby/6kyu_stop_gninnips_my_sdrow.rb
@@ -0,0 +1,42 @@
+# https://www.codewars.com/kata/stop-gninnips-my-sdrow/train/ruby
+
+require 'ap'
+
+def spinWords(string)
+  array = string.split(" ")
+  new_array = array.each do |word|
+    if word.length >= 5
+      word.reverse!
+    end
+  end
+  return new_array.join(" ").to_s
+end
+
+answer = spinWords("Hey fellow warriors")
+ap answer
+# "Hey wollef sroirraw"
+
+answer = spinWords( "This is a test")
+ap answer
+# "This is a test"
+
+answer = spinWords( "This is another test" )
+ap answer
+# "This is rehtona test"
+
+# Another Way
+def spinWords(string)
+  string.gsub(/\w{5,}/, &:reverse)
+end
+
+answer = spinWords("Hey fellow warriors")
+ap answer
+# "Hey wollef sroirraw"
+
+answer = spinWords( "This is a test")
+ap answer
+# "This is a test"
+
+answer = spinWords( "This is another test" )
+ap answer
+# "This is rehtona test"
--- ruby/7kyu_no_oddities_here.rb
+++ ruby/7kyu_no_oddities_here.rb
@@ -12,14 +12,35 @@ def no_odds(values)
   return new_array
 end
 
-describe "Solution" do
-  it "should return no_odd values correctly" do
-    Test.assert_equals(no_odds([11,2,8,7,4]), [2,8,4])
-    Test.assert_equals(no_odds([32,27,52,68,86,12,10,9]), [32,52,68,86,12,10])
-    Test.assert_equals(no_odds([749,432,6643,632,1,2,3,4,5]), [432,632,2,4])
-    Test.assert_equals(no_odds([22,31,56,1,2,3,4,5,6,7]), [22,56,2,4,6])
-    Test.assert_equals(no_odds([1]), [])
-  end
+# describe "Solution" do
+#   it "should return no_odd values correctly" do
+#     Test.assert_equals(no_odds([11,2,8,7,4]), [2,8,4])
+#     Test.assert_equals(no_odds([32,27,52,68,86,12,10,9]), [32,52,68,86,12,10])
+#     Test.assert_equals(no_odds([749,432,6643,632,1,2,3,4,5]), [432,632,2,4])
+#     Test.assert_equals(no_odds([22,31,56,1,2,3,4,5,6,7]), [22,56,2,4,6])
+#     Test.assert_equals(no_odds([1]), [])
+#   end
+# end
+
+answer = no_odds([11,2,8,7,4])
+ap answer
+
+answer = no_odds([32,27,52,68,86,12,10,9])
+ap answer
+
+answer = no_odds([749,432,6643,632,1,2,3,4,5])
+ap answer
+
+answer = no_odds([22,31,56,1,2,3,4,5,6,7])
+ap answer
+
+answer = no_odds([1])
+ap answer
+
+# Another Way
+def no_odds(values)
+  values.delete_if { |i| i % 2 != 0 }
+  return values
 end
 
 answer = no_odds([11,2,8,7,4])
