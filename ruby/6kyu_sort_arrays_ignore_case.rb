7b967792e3f7391e301db562182a58922599e14e
Sherilyn Moline <smoline@gmail.com>
2017-07-27 20:20:40 -0400

    6kyu sort arrays ignoring case

---
+++ ruby/6kyu_sort_arrays_ignore_case.rb
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/sort-arrays-ignoring-case/train/ruby
+
+require 'ap'
+
+def sortme(names)
+  return names.sort_by(&:downcase)
+end
+
+answer = sortme([ "Hello", "there", "I'm", "fine"])
+ap answer
+# ["fine", "Hello", "I'm", "there" ]
