8c0dbe5ad65fa3c60ff53546728a2240b0bf01e0
Sherilyn Moline <smoline@gmail.com>
2017-08-18 09:56:15 -0400

    update with another answer

--- ruby/7kyu_incrementer.rb
+++ ruby/7kyu_incrementer.rb
@@ -25,3 +25,28 @@ ap answer
 answer = incrementer([1,2,3,4,5,6,7,8,9,9,9,9,9,8])
 ap answer
 # [2,4,6,8,0,2,4,6,8,9,0,1,2,2]
+
+# Another Way
+def incrementer(nums)
+  nums.map.with_index(1){|num, idx| (num+idx)%10}
+end
+
+answer = incrementer([])
+ap answer
+# []
+
+answer = incrementer([1,2,3])
+ap answer
+# [2,4,6]
+
+answer = incrementer([4,6,7,1,3])
+ap answer
+# [5,8,0,5,8]
+
+answer = incrementer([3,6,9,8,9])
+ap answer
+# [4,8,2,2,4]
+
+answer = incrementer([1,2,3,4,5,6,7,8,9,9,9,9,9,8])
+ap answer
+# [2,4,6,8,0,2,4,6,8,9,0,1,2,2]
