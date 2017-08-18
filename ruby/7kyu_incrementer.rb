4d44b048b0e1829997fe116a22578cd460f72ff1
Sherilyn Moline <smoline@gmail.com>
2017-08-18 09:54:40 -0400

    7kyu Incrementer

---
+++ ruby/7kyu_incrementer.rb
@@ -0,0 +1,27 @@
+# https://www.codewars.com/kata/incrementer
+
+require 'ap'
+
+def incrementer(num)
+  num.each_with_index.map { |x, i| (x + (i + 1)).to_s[-1].to_i}
+end
+
+answer = incrementer([])
+ap answer
+# []
+
+answer = incrementer([1,2,3])
+ap answer
+# [2,4,6]
+
+answer = incrementer([4,6,7,1,3])
+ap answer
+# [5,8,0,5,8]
+
+answer = incrementer([3,6,9,8,9])
+ap answer
+# [4,8,2,2,4]
+
+answer = incrementer([1,2,3,4,5,6,7,8,9,9,9,9,9,8])
+ap answer
+# [2,4,6,8,0,2,4,6,8,9,0,1,2,2]
