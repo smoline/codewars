6ee8ef1e21671ed340beff8bc4fc0e062cbc1331
Sherilyn Moline <smoline@gmail.com>
2018-09-14 14:51:48 -0400

    codewars ruby practice

---
+++ ruby/6kyu_bit_counting.rb
@@ -0,0 +1,25 @@
+require 'ap'
+
+def count_bits(n)
+    n.to_s(2).chars.count("1")
+end
+
+answer = count_bits(5)
+ap answer
+# 2
+
+answer = count_bits(9)
+ap answer
+# 2
+
+answer = count_bits(8)
+ap answer
+# 1
+
+answer = count_bits(6)
+ap answer
+# 2
+
+answer = count_bits(4)
+ap answer
+# 1
