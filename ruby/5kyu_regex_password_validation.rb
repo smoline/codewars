b5b696217c60efc4092b6fa1e358cbc3b1514985
Sherilyn Moline <smoline@gmail.com>
2018-09-17 13:43:41 -0400

    5kyu regex password validation

---
+++ ruby/5kyu_regex_password_validation.rb
@@ -0,0 +1,32 @@
+# https://www.codewars.com/kata/regex-password-validation/train/ruby
+
+require 'ap'
+
+def validate_password(password)
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/ === password ||
+  /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])^[a-zA-Z0-9]{6,}$/ === password
+end
+
+answer = validate_password('ghdfj32')
+ap answer
+# false
+
+answer = validate_password('DSJKHD23')
+ap answer
+# false
+
+answer = validate_password('4fdg5Fj3')
+ap answer
+# true
+
+answer = validate_password('fjd3IR9.;')
+ap answer
+# false
+
+answer = validate_password('a2.d412')
+ap answer
+# false
+
+answer = validate_password('123abcABC')
+ap answer
+# true
