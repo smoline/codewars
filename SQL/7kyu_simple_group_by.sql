fdd2e43134512d6936a90a0f8c186d050ca1f89b
Sherilyn Moline <smoline@gmail.com>
2018-10-04 13:37:06 -0400

    7kyu group by sql

---
+++ SQL/7kyu_simple_group_by.sql
@@ -0,0 +1,3 @@
+/* https://www.codewars.com/kata/sql-basics-simple-group-by/train/sql */
+
+SELECT COUNT(age) AS people_count, age FROM people GROUP BY age
---
+++ SQL/8kyu_simple_min_max.sql
@@ -0,0 +1,3 @@
+/* https://www.codewars.com/kata/sql-basics-simple-min-slash-max/train/sql */
+
+SELECT MIN(age) AS age_min, MAX(age) AS age_max FROM people
