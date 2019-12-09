f4958dddcabb24cf42229ec8b419d05524a56ef1
Sherilyn Moline <smoline@gmail.com>
2019-12-09 13:01:50 -0500

    7kyu vowel count

---
+++ javascript/7kyu_vowel_count.js
@@ -0,0 +1,32 @@
+// https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/javascript
+
+function getCount(str) {
+  var vowelsCount = 0
+  const vowels = ["a", "e", "i", "o", "u"]
+
+  var str_arr = Array.from(str)
+  
+  str_arr.map((letter) => {
+    if (vowels.includes(letter)) ++vowelsCount
+  })
+
+  return vowelsCount
+}
+
+console.log(getCount("abracadabra"))
+// 5
+
+// refactored
+function getCount1(str) {
+  var vowelsCount = 0
+  const vowels = ["a", "e", "i", "o", "u"]
+
+  Array.from(str).map((letter) => {
+    if (vowels.includes(letter)) ++vowelsCount
+  })
+
+  return vowelsCount
+}
+
+console.log(getCount1("abracadabra"))
+// 5
