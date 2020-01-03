dad91ebf108de3e17d2667025ab3d6985da3f6a6
Sherilyn Moline <smoline@gmail.com>
2020-01-03 17:08:57 -0500

    rubocop updates

--- ruby/6kyu_tortoise_racing.rb
+++ ruby/6kyu_tortoise_racing.rb
@@ -31,49 +31,49 @@ ap answer
 # nil
 
 # Another Way
-def race(v1, v2, g)
+def race1 (v1, v2, g)
   return nil if v2 < v1
-  t = (g.to_f/(v2.to_f - v1.to_f))
-  s = ((t.to_f*3600))%60
-  m = ((t.to_f)*60.00)%60
-  h = (t.to_f)%60
+  t = (g.to_f / (v2.to_f - v1.to_f))
+  s = ((t.to_f * 3600)) % 60
+  m = ((t.to_f) * 60.00) % 60
+  h = (t.to_f) % 60
   [h.floor, m.floor, s.floor]
 end
 
-answer = race(720, 850, 70)
+answer = race1(720, 850, 70)
 ap answer
 # [0, 32, 18]
 
-answer = race(80, 91, 37)
+answer = race1(80, 91, 37)
 ap answer
 # [3, 21, 49]
 
-answer = race(80, 100, 40)
+answer = race1(80, 100, 40)
 ap answer
 # [2, 0, 0]
 
-answer = race(80, 70, 40)
+answer = race1(80, 70, 40)
 ap answer
 # nil
 
 # Another Way
-def race(v1, v2, g)
+def race2(v1, v2, g)
   sec = (g * 3600 / (v2 - v1))
-  [sec / 3600,  (sec / 60) % 60, sec % 60] if v2 > v1
+  [sec / 3600, (sec / 60) % 60, sec % 60] if v2 > v1
 end
 
-answer = race(720, 850, 70)
+answer = race2(720, 850, 70)
 ap answer
 # [0, 32, 18]
 
-answer = race(80, 91, 37)
+answer = race2(80, 91, 37)
 ap answer
 # [3, 21, 49]
 
-answer = race(80, 100, 40)
+answer = race2(80, 100, 40)
 ap answer
 # [2, 0, 0]
 
-answer = race(80, 70, 40)
+answer = race2(80, 70, 40)
 ap answer
 # nil
